f6a3aae3b485d23f3a517792308ad013e9df292a
zykuriku <kavyapandey2002@gmail.com>
2024-07-31 01:40:47 +0530

    added firebase

--- lib/screens/first page.dart
+++ lib/screens/first page.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/src/extension_navigation.20%dart';
+
 class FirstPage extends StatelessWidget {
   const FirstPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: FloatingActionButton(onPressed: (){
-        Get.toNamed('/map');
-      },
-      child: Text('click me'),),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(onPressed: (){
+            Get.toNamed('/map');
+          },
+          child: Center(child: Text('click me')),),
+        ],
+      ),
     );
   }
 }
--- lib/screens/login page.dart
+++ lib/screens/login page.dart
@@ -1,5 +1,7 @@
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 
 
 class LoginScreen extends StatefulWidget {
@@ -13,121 +15,118 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: bgColor,
-      // body: Container(
-      //   decoration: BoxDecoration(
-      //       gradient: LinearGradient(
-      //           colors: [bgColor, gColor, nColor],
-      //           begin: Alignment.topCenter,
-      //           end: Alignment.bottomCenter)),
-      //   child: Padding(
-      //     padding: EdgeInsets.symmetric(horizontal: 24.0),
-      //     child: Column(
-      //       mainAxisAlignment: MainAxisAlignment.center,
-      //       crossAxisAlignment: CrossAxisAlignment.center,
-      //       children: <Widget>[
-      //         Column(
-      //           crossAxisAlignment: CrossAxisAlignment.start,
-      //           children: [
-      //             Text(
-      //               'Look!',
-      //               style: TextStyle(fontSize: 70.0),
-      //             ),
-      //             Text(
-      //               'who\'s here',
-      //               style: TextStyle(fontSize: 34.0),
-      //             ),
-      //             Text(
-      //               'once again~',
-      //               style: TextStyle(fontSize: 26.0),
-      //             ),
-      //           ],
-      //         ),
-      //         const SizedBox(
-      //           height: 48.0,
-      //         ),
-      //         TextField(
-      //           textAlign: TextAlign.center,
-      //           keyboardType: TextInputType.emailAddress,
-      //           onChanged: (value) {
-      //             email = value;
-      //           },
-      //           decoration: const InputDecoration(
-      //             hintText: 'Enter your email',
-      //             contentPadding:
-      //             EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-      //             border: OutlineInputBorder(
-      //               borderRadius: BorderRadius.all(Radius.circular(32.0)),
-      //             ),
-      //             enabledBorder: OutlineInputBorder(
-      //               borderSide: BorderSide(color: Colors.brown, width: 1.0),
-      //               borderRadius: BorderRadius.all(Radius.circular(32.0)),
-      //             ),
-      //             focusedBorder: OutlineInputBorder(
-      //               borderSide: BorderSide(color: Colors.brown, width: 2.0),
-      //               borderRadius: BorderRadius.all(Radius.circular(32.0)),
-      //             ),
-      //           ),
-      //         ),
-      //         const SizedBox(
-      //           height: 8.0,
-      //         ),
-      //         TextField(
-      //           textAlign: TextAlign.center,
-      //           onChanged: (value) {
-      //             pass = value;
-      //           },
-      //           obscureText: true,
-      //           decoration: const InputDecoration(
-      //             hintText: 'Enter your password.',
-      //             contentPadding:
-      //             EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-      //             border: OutlineInputBorder(
-      //               borderRadius: BorderRadius.all(Radius.circular(32.0)),
-      //             ),
-      //             enabledBorder: OutlineInputBorder(
-      //               borderSide: BorderSide(color: Colors.brown, width: 1.0),
-      //               borderRadius: BorderRadius.all(Radius.circular(32.0)),
-      //             ),
-      //             focusedBorder: OutlineInputBorder(
-      //               borderSide: BorderSide(color: Colors.brown, width: 2.0),
-      //               borderRadius: BorderRadius.all(Radius.circular(32.0)),
-      //             ),
-      //           ),
-      //         ),
-      //         const SizedBox(
-      //           height: 24.0,
-      //         ),
-      //         Padding(
-      //           padding: EdgeInsets.symmetric(vertical: 16.0),
-      //           child: Material(
-      //             color: Colors.brown,
-      //             borderRadius: BorderRadius.all(Radius.circular(30.0)),
-      //             elevation: 5.0,
-      //             child: MaterialButton(
-      //               onPressed: () async {
-      //                 await FirebaseAuth.instance.signInWithEmailAndPassword(
-      //                     email: email, password: pass);
-      //                 if (FirebaseAuth.instance.currentUser != null) {
-      //                   Navigator.push(
-      //                       context,
-      //                       MaterialPageRoute(
-      //                           builder: (context) => HomePage()));
-      //                 }
-      //               },
-      //               minWidth: 200.0,
-      //               height: 42.0,
-      //               child: const Text(
-      //                 'Log In',
-      //                 style: TextStyle(color: Colors.white),
-      //               ),
-      //             ),
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-      // ),
+      backgroundColor: Colors.white,
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.white],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter)),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Look!',
+                    style: TextStyle(fontSize: 70.0),
+                  ),
+                  Text(
+                    'who\'s here',
+                    style: TextStyle(fontSize: 34.0),
+                  ),
+                  Text(
+                    'once again~',
+                    style: TextStyle(fontSize: 26.0),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: const InputDecoration(
+                  hintText: 'Enter your email',
+                  contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.brown, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.brown, width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  pass = value;
+                },
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: 'Enter your password.',
+                  contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.brown, width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.brown, width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                child: Material(
+                  color: Colors.brown,
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  elevation: 5.0,
+                  child: MaterialButton(
+                    onPressed: () async {
+                      await FirebaseAuth.instance.signInWithEmailAndPassword(
+                          email: email, password: pass);
+                      if (FirebaseAuth.instance.currentUser != null) {
+                        Get.toNamed("/verify");
+                      }
+                    },
+                    minWidth: 200.0,
+                    height: 42.0,
+                    child: const Text(
+                      'Log In',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
